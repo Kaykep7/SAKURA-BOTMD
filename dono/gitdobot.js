12d79dad19d448e303463e233c5367536708ef0d
Kaykep7 <kaykep7@gmail.com>
2022-12-18 01:07:59 +0000

    jdhjfh

---
+++ dono/gitdobot.js
@@ -0,0 +1,7 @@
+const gitdobot = (prefix) => {
+return`
+*SÓ NO CANAL DO MEU CRIADOR ^-^ LÁ TEM O VÍDEO, O LINK DO BOT TA NOS COMENTÁRIOS FIXADO :) https://youtube.com/@MarcioScheyot*
+`
+}
+
+exports.gitdobot = gitdobot
